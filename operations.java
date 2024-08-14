049f2301b4299990f2282c9f335e5218f191056b
Elijah <elijahchinnam@gmail.com>
2024-08-14 23:45:13 +0530

    I'm Elijah and I added a Java file that performs operations

---
+++ operations.java
@@ -0,0 +1,13 @@
+public class operations {
+
+    public static void main(String[] args) {
+        sum in = new sum();
+        in.main(args);
+
+        prod in2 = new prod();
+        in2.main(args);
+        diff in3 = new diff();
+        in3.main(args);
+    }
+    
+}
